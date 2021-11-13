7b21ba2eea92bc0eab691d1ea86c4db4ac081c19
Fiji404 <87615863+Fiji404@users.noreply.github.com>
2021-11-13 20:18:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 var tips = document.getElementById('tip_abbr');
 var mutablecontent = document.querySelector('.tips-content');
 var contentshow = document.querySelector('.show');
-    tips.addEventListener('click', function (event) {
-        mutablecontent.classList.toggle('show')
-    }, 1000);
+var discord = document.querySelector('.discord');
+var dcinfo = document.querySelector('.discord-info')
+    tips.addEventListener('click', function() {
+        mutablecontent.classList.toggle('show');
+});
+
+discord.addEventListener('click', function() {
+    dcinfo.classList.toggle('show-info')
+})
