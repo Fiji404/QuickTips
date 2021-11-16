821425e20776354cfcae815438bba346065509e2
Fiji404 <87615863+Fiji404@users.noreply.github.com>
2021-11-16 19:47:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ let discord = document.getElementById('discord');
 let dcinfo = document.querySelector('.discord-info');
 let infocat = document.querySelector('.category-info');
 let column = document.querySelector('.links-wrapper')
-let alternate = document.querySelector('.footer');
+let aside = document.querySelector('.aside');
+let asidebtn = document.querySelector('.aside-toggle')
     tips.addEventListener('click', function() {
         mutablecontent.classList.toggle('show');
 });
 
 discord.addEventListener('click', function() {
-    dcinfo.classList.toggle('show-info')
+    dcinfo.classList.toggle('show-info');
+});
+
+asidebtn.addEventListener('click', function() {
+    aside.classList.toggle('active');
+    asidebtn.textContent = aside.classList.contains('active') ? '<' : '>';
 })
+
 
