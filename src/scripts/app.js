3e9e4ffd38f9193359145ad116a71eadea3125f3
Fiji404 <radek42310o@gmail.com>
2022-04-17 20:28:43 +0200

    nothing

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,63 +1,63 @@
-const tipsToggle = document.querySelector('.tips--toggle');
-const tipsContent = document.querySelector('.tips-section');
-const accessibilityToggle = document.querySelector('.availability_toggle');
-const accessibilityContent = document.querySelector('.availability-section');
-const tipsCards = document.querySelectorAll('.card-tip');
-const containerCategory = document.querySelector('.container-tips');
-const hideTip = document.querySelector('.tip-card_hide');
-const infoBoxExpander = document.querySelectorAll('.availability-content_expander');
+const tipsToggle = document.querySelector(".tips--toggle");
+const tipsContent = document.querySelector(".tips-section");
+const accessibilityToggle = document.querySelector(".availability_toggle");
+const accessibilityContent = document.querySelector(".availability-section");
+const tipsCards = document.querySelectorAll(".card-tip");
+const containerCategory = document.querySelector(".container-tips");
+const hideTip = document.querySelector(".tip-card_hide");
+const infoBoxExpander = document.querySelectorAll(".availability-content_expander");
 
-tipsToggle.addEventListener('click', () => {
-    tipsContent.classList.toggle('active');
-    if (accessibilityContent.classList.contains('active')) {
-      accessibilityContent.classList.remove('active');
+tipsToggle.addEventListener("click", () => {
+    tipsContent.classList.toggle("active");
+    if (accessibilityContent.classList.contains("active")) {
+        accessibilityContent.classList.remove("active");
     }
 });
 
-accessibilityToggle.addEventListener('click', () => {
-  accessibilityContent.classList.toggle('active');
-  if (tipsContent.classList.contains('active')) {
-    tipsContent.classList.remove('active');
-  };
+accessibilityToggle.addEventListener("click", () => {
+    accessibilityContent.classList.toggle("active");
+    if (tipsContent.classList.contains("active")) {
+        tipsContent.classList.remove("active");
+    }
 });
 
-containerCategory.addEventListener('click', (e) => {
-  tipsCards.forEach((el) => {
-      if (el.classList.contains('active')) return;
+containerCategory.addEventListener("click", (e) => {
+    tipsCards.forEach((el) => {
+        if (el.classList.contains("active")) return;
     });
 });
 
 tipsCards.forEach((el) => {
-  el.addEventListener('click', () => {
-    el.classList.add('active');
+    el.addEventListener("click", () => {
+        el.classList.add("active");
 
-    tipsCards.forEach((element) => {
-      if (element.classList.contains('active')) {
-        hideTip.classList.add('visible');
-        return;
-      }
-      element.classList.add('hide');
+        tipsCards.forEach((element) => {
+            if (element.classList.contains("active")) {
+                hideTip.classList.add("visible");
+                return;
+            }
+            element.classList.add("hide");
+        });
     });
-
-  });
 });
 
-hideTip.addEventListener('click', () => {
-  tipsCards.forEach((el) => {
-    el.classList.remove('hide');
-    el.classList.remove('active');
-    hideTip.classList.remove('visible');
-  });
+hideTip.addEventListener("click", () => {
+    tipsCards.forEach((el) => {
+        el.classList.remove("hide");
+        el.classList.remove("active");
+        hideTip.classList.remove("visible");
+    });
 });
 
-for (let i = 0; i < infoBoxExpander.length; i++) {
-  infoBoxExpander[i].addEventListener('click', function() {
-    this.classList.toggle('active');
-    let contentBox = this.nextElementSibling;
-    if (contentBox.style.maxHeight) {
-      contentBox.style.maxHeight = null;
-    } else {
-      contentBox.style.maxHeight = contentBox.scrollHeight + "px";
-    }
-  });
-};
+
+infoBoxExpander.forEach((element) => {
+    element.addEventListener("click", () => {
+        element.classList.toggle("active");
+        const infoExpand = element.nextElementSibling;
+        if (infoExpand.style.maxHeight) {
+            infoExpand.style.maxHeight = null;
+        } else {
+            infoExpand.style.maxHeight = `${infoExpand.scrollHeight}px`;
+        }
+    });
+});
