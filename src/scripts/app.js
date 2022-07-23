7199b56ba2272af16f4f989ccc8f14c5318ab15c
Fiji <radek42310o@gmail.com>
2022-07-23 13:09:43 +0200

    change navigation bar, remove unecessary code

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,58 +1,57 @@
-const tipsToggle = document.querySelector(".tips--toggle");
-const tipsContent = document.querySelector(".tips-section");
-const accessibilityToggle = document.querySelector(".availability_toggle");
-const accessibilityContent = document.querySelector(".availability-section");
-const tipsCards = document.querySelectorAll(".card-tip");
-const containerCategory = document.querySelector(".container-tips");
-const hideTip = document.querySelector(".tip-card_hide");
-const infoBoxExpander = document.querySelectorAll(".availability-content_expander");
+const tipsToggle = document.querySelector('.tips--toggle');
+const tipsContent = document.querySelector('.tips-section');
+const accessibilityToggle = document.querySelector('.availability_toggle');
+const accessibilityContent = document.querySelector('.availability-section');
+const tipsCards = document.querySelectorAll('.card-tip');
+const containerCategory = document.querySelector('.container-tips');
+const hideTip = document.querySelector('.tip-card_hide');
+const infoBoxExpander = document.querySelectorAll('.availability-content_expander');
 
-tipsToggle.addEventListener("click", () => {
-    tipsContent.classList.toggle("active");
-    if (accessibilityContent.classList.contains("active")) {
-        accessibilityContent.classList.remove("active");
+tipsToggle.addEventListener('click', () => {
+    tipsContent.classList.toggle('active');
+    if (accessibilityContent.classList.contains('active')) {
+        accessibilityContent.classList.remove('active');
     }
 });
 
-accessibilityToggle.addEventListener("click", () => {
-    accessibilityContent.classList.toggle("active");
-    if (tipsContent.classList.contains("active")) {
-        tipsContent.classList.remove("active");
+accessibilityToggle.addEventListener('click', () => {
+    accessibilityContent.classList.toggle('active');
+    if (tipsContent.classList.contains('active')) {
+        tipsContent.classList.remove('active');
     }
 });
 
-containerCategory.addEventListener("click", (e) => {
-    tipsCards.forEach((el) => {
-        if (el.classList.contains("active")) return;
+containerCategory.addEventListener('click', e => {
+    tipsCards.forEach(el => {
+        if (el.classList.contains('active')) return;
     });
 });
 
-tipsCards.forEach((el) => {
-    el.addEventListener("click", () => {
-        el.classList.add("active");
+tipsCards.forEach(el => {
+    el.addEventListener('click', () => {
+        el.classList.add('active');
 
-        tipsCards.forEach((element) => {
-            if (element.classList.contains("active")) {
-                hideTip.classList.add("visible");
+        tipsCards.forEach(element => {
+            if (element.classList.contains('active')) {
+                hideTip.classList.add('visible');
                 return;
             }
-            element.classList.add("hide");
+            element.classList.add('hide');
         });
     });
 });
 
-hideTip.addEventListener("click", () => {
-    tipsCards.forEach((el) => {
-        el.classList.remove("hide");
-        el.classList.remove("active");
-        hideTip.classList.remove("visible");
+hideTip.addEventListener('click', () => {
+    tipsCards.forEach(el => {
+        el.classList.remove('hide');
+        el.classList.remove('active');
+        hideTip.classList.remove('visible');
     });
 });
 
-
-infoBoxExpander.forEach((element) => {
-    element.addEventListener("click", () => {
-        element.classList.toggle("active");
+infoBoxExpander.forEach(element => {
+    element.addEventListener('click', () => {
+        element.classList.toggle('active');
         const infoExpand = element.nextElementSibling;
         if (infoExpand.style.maxHeight) {
             infoExpand.style.maxHeight = null;
--- src/scripts/themeOption.js
+++ src/scripts/themeOption.js
@@ -1,23 +1,39 @@
 const changeThemeBtn = document.querySelector('.theme-toggle-btn__dot');
+const navToggleBtn = document.querySelector('.nav-toggle-btn');
+const navigationBar = document.querySelector('.nav-list');
 const bodyElement = document.body;
 const themePreference = localStorage.getItem('theme');
 
 const checkUserPreferences = () => {
-  if (themePreference === 'light') {
-    changeThemeBtn.classList.add('active');
-    bodyElement.classList.replace('dark', 'light')
-  }
-}
+    if (themePreference === 'light') {
+        changeThemeBtn.classList.add('active');
+        bodyElement.classList.replace('dark', 'light');
+    }
+};
 
 changeThemeBtn.addEventListener('click', () => {
     if (bodyElement.classList.contains('dark')) {
-      changeThemeBtn.classList.add('active');
-      bodyElement.classList.replace('dark', 'light');
-      localStorage.setItem('theme', 'light');
+        changeThemeBtn.classList.add('active');
+        bodyElement.classList.replace('dark', 'light');
+        localStorage.setItem('theme', 'light');
     } else {
-      bodyElement.classList.replace('light', 'dark');
-      localStorage.removeItem('theme');
-      changeThemeBtn.classList.remove('active');
+        bodyElement.classList.replace('light', 'dark');
+        localStorage.removeItem('theme');
+        changeThemeBtn.classList.remove('active');
+    }
+});
+
+navToggleBtn.addEventListener('click', () => {
+    navToggleBtn.classList.toggle('active');
+    navigationBar.classList.toggle('active');
+    bodyElement.classList.toggle('disabled');
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 700) {
+        bodyElement.classList.remove('disabled');
+        navToggleBtn.classList.remove('active');
+        navigationBar.classList.remove('active');
     }
 });
 
