01b0db14262505ff1f5591e2aaddc776ef5270ee
Fiji <radek42310o@gmail.com>
2022-07-28 16:09:54 +0200

    add register feature

--- src/scripts/loginPage.js
+++ src/scripts/loginPage.js
@@ -1,4 +1,19 @@
 'use strict';
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth';
+import { getDatabase, ref, update, get } from 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyCc1rgGJxFGcZTJL7AADjy2exF1-aN3-wU',
+    authDomain: 'quick-tips-app.firebaseapp.com',
+    projectId: 'quick-tips-app',
+    messagingSenderId: '867547460819',
+    appId: '1:867547460819:web:ab8854d60ad3627e191026',
+};
+
+const app = initializeApp(firebaseConfig);
+const DB = ref(getDatabase());
+const auth = getAuth();
 
 const formInputElements = document.querySelectorAll('.login-form__input');
 const passwordShowBtn = document.querySelector('.password-section__show-password-btn');
@@ -27,3 +42,67 @@ formInputElements.forEach(input => {
         parentElement.classList.remove('focused');
     });
 });
+
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const emailInput = document.getElementById('email-section__email-input').value;
+    const passwordInput = document.getElementById('password-section__password-input').value;
+    signInWithEmailAndPassword(auth, emailInput, passwordInput)
+        .then(userCredential => {
+            const user = userCredential.username;
+            const date = new Intl.DateTimeFormat('pl-PL', { dateStyle: 'long' }).format();
+            console.log(user);
+            update(DB, 'users/' + user, {
+                lastTimeLogin: date,
+            });
+        })
+        .catch(error => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+});
+
+// const logoutButton = document.querySelector('.userLoggedIn_logout');
+// const userLoggedUsername = document.querySelector('.userLoggedIn__title');
+// const loginLink = document.querySelector('.login-item');
+// const userButton = document.querySelector('.userLoggedIn');
+// const user = auth.currentUser;
+// onAuthStateChanged(auth, user => {
+//     const logoutIcon = document.querySelector('.userLoggedIn__icon');
+//     userButton.addEventListener('click', () => {
+//         logoutButton.classList.toggle('active');
+//         logoutIcon.classList.toggle('active');
+//     });
+
+//     if (user) {
+//         loginLink.classList.add('hidden');
+//         userButton.classList.add('active');
+//         // userLoggedUsername.textContent = user.uid;
+//         const getUser = ref(database, 'users/' + userUsername);
+//         onValue(getUser, snapshot => {
+//             const data = snapshot.val();
+//             console.log(data);
+//             userLoggedUsername = data.email;
+//         });
+
+//         // ...
+//     } else {
+//         // User is signed out
+//         // ...
+//     }
+// });
+
+// logoutButton.addEventListener('click', () => {
+//     signOut(auth)
+//         .then(() => {
+//             alert('signout success');
+//             userLoggedUsername.textContent = '';
+//             loginLink.classList.remove('hidden');
+//             userButton.classList.remove('active');
+//         })
+//         .catch(error => {
+//             // An error happened.
+//         });
+// });
--- src/scripts/registerPage.js
+++ src/scripts/registerPage.js
@@ -1,49 +1,49 @@
 'use strict';
 import { initializeApp } from 'firebase/app';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth';
-import { getDatabase, set, ref, update, onValue } from 'firebase/database';
+import { getDatabase, set, ref, update, onValue, child } from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyCc1rgGJxFGcZTJL7AADjy2exF1-aN3-wU',
-    authDomain: 'quick-tips-app.firebaseapp.com',
-    projectId: 'quick-tips-app',
-    storageBucket: 'quick-tips-app.appspot.com',
-    messagingSenderId: '867547460819',
-    appId: '1:867547460819:web:ab8854d60ad3627e191026',
-};
+    apiKey: "AIzaSyCc1rgGJxFGcZTJL7AADjy2exF1-aN3-wU",
+    authDomain: "quick-tips-app.firebaseapp.com",
+    databaseURL: "https://quick-tips-app-default-rtdb.firebaseio.com",
+    projectId: "quick-tips-app",
+    storageBucket: "quick-tips-app.appspot.com",
+    messagingSenderId: "867547460819",
+    appId: "1:867547460819:web:ab8854d60ad3627e191026",
+    measurementId: "G-TWB6CE3N9N"
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
-const database = getDatabase();
+const db = getDatabase();
 
-const passwordInputElement = document.querySelector('.input-container__input_password');
-const passwordRepeatInputElement = document.querySelector('.input-container__input_password-repeat');
 const registerForm = document.querySelector('.register-form');
 
 registerForm.addEventListener('submit', ev => {
-    e.preventDefault();
+    ev.preventDefault();
     const usernameInput = document.getElementById('register-form__username').value;
     const passwordInput = document.getElementById('register-form__password').value;
     const repeatPasswordInput = document.getElementById('register-form__password-repeat').value;
     const emailInput = document.getElementById('register-form__email').value;
-    if (passwordInputElement.value !== passwordRepeatInputElement.value && passwordInputElement.value && passwordRepeatInputElement.value) {
-        ev.preventDefault();
-        createUserWithEmailAndPassword(auth, userEmail, passwordInput)
+    if (passwordInput === repeatPasswordInput && passwordInput && repeatPasswordInput) {
+        createUserWithEmailAndPassword(auth, emailInput, passwordInput)
             .then(userCredential => {
-                signUpForm.reset();
+                console.log(userCredential);
                 const user = userCredential.user;
-                set(ref(database, 'users/' + user.uid), {
-                    Username: usernameInput,
-                    Password: passwordInput,
-                    Email: emailInput,
-                    accountCreationDate: new Intl.DateTimeFormat('pl-PL', { dateStyle: 'long' }).format()
+                set(ref(db, 'users/' + user.uid), {
+                    username: usernameInput,
+                    password: passwordInput,
+                    email: emailInput,
+                    accountCreationDate: new Intl.DateTimeFormat('en-US', { dateStyle: 'long', timeStyle: 'short' }).format()
                 });
+                registerForm.reset();
+                alert('correct')
             })
             .catch(error => {
                 // const errorMessage = error.message;
                 const errorCode = error.code;
-                errorPopUp.classList.add('active');
-                errorText.textContent = `Error Code: ${errorCode}`;
+                console.log(errorCode);
             });
     }
 });
