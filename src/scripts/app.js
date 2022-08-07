37dd9d9941c6a921bf1a9710b1209cefb75b52f4
Fiji <radek42310o@gmail.com>
2022-08-07 10:00:13 +0200

    remove all the tips stored in UI for rendering it from the server

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,44 @@
+import { getDatabase, ref, get, set, child } from 'firebase/database';
+
 const pageSectionLinks = document.querySelector('.available-tips-intro');
 const pageSections = [...document.querySelectorAll('.tips-section, .javascript-section')];
 const tipsContainer = document.querySelector('.container-tips');
 const returnToAllTipsBtn = document.querySelector('.tip-card_hide');
-const tipCards = document.querySelectorAll('.card-tip');
+const db = ref(getDatabase());
+
+const renderTipsInUI = (title, desc, src) => {
+    const tipTemplateStructure = `
+    <section style="--tipCard-delay: 0.2s" class="card-tip">
+        <button class="card-tip__button">${title}</button>
+        <section class="hidden-content">
+        <p class="hidden-content__text">
+            ${desc}
+        </p>
+        <iframe
+            class="hidden-content__iframe"
+            scrolling="no"
+            title="REM and EM comparsion"
+            src=${src}
+            frameborder="no"
+            loading="lazy"
+            allowtransparency="true"
+            allowfullscreen="true"
+        >
+        </iframe>
+        </section>
+    </section>`;
+    tipsContainer.insertAdjacentHTML('beforeend', tipTemplateStructure);
+};
+
+const getTipsFromDB = () => {
+    get(child(db, 'tips/')).then(tips => {
+        const tipsArray = Object.values(tips.val());
+        tipsArray.forEach(tip => {
+            const { title, description, source } = tip;
+            renderTipsInUI(title, description, source);
+        });
+    });
+};
 
 pageSectionLinks.addEventListener('click', ({ target, currentTarget }) => {
     if (target !== currentTarget) {
@@ -21,9 +57,9 @@ tipsContainer.addEventListener('click', e => {
     if (e.target.matches('.card-tip__button')) {
         const tipCard = e.target.closest('.card-tip');
         tipCard.classList.toggle('active');
-        tipCards.forEach(el => {
+        document.querySelectorAll('.card-tip').forEach(el => {
             if (el !== tipCard) {
-                el.classList.add('tip-card_hide');
+                el.classList.add('hide');
             }
         });
         returnToAllTipsBtn.classList.add('active');
@@ -31,8 +67,12 @@ tipsContainer.addEventListener('click', e => {
 });
 
 returnToAllTipsBtn.addEventListener('click', () => {
-    tipCards.forEach(el => {
-        el.classList.remove('tip-card_hide', 'active');
+    document.querySelectorAll('.card-tip').forEach(el => {
+        el.classList.remove('hide', 'active');
     });
     returnToAllTipsBtn.classList.remove('active');
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    getTipsFromDB();
 });
